980744c3892d1e0215a863e0d6e655db9bc72238
Coruscant11 <coruscant11@gmail.com>
2016-03-13 22:06:29 +0100

    Optimisation

--- OSBLCDown/Program.cs
+++ OSBLCDown/Program.cs
@@ -12,57 +12,84 @@ namespace OSBLCDown
     {
         static void Main(string[] args)
         {
-            int time = int.Parse(System.IO.File.ReadAllText("config.txt"));
+            int time = int.Parse(System.IO.File.ReadAllText("config.txt")); // Récupère l'intervalle de vérification dans le fichier config.txt
 
             Console.WriteLine("Lancement du programme avec pour paramètre " + time.ToString() + " secondes.\nAppuyez sur entrée pour valider...\n");
             Console.ReadLine();
 
             do
             {
+                Console.WriteLine("------------------------------------------------");
+
                 try
                 {
-                    MinimalisticTelnet.TelnetConnection connection = new MinimalisticTelnet.TelnetConnection("server.osblc.fr", 10011);
-                    if (connection.Read().Length > 2)
+                    Process();
+                } catch (Exception e) {
+
+                    Console.WriteLine("Echec de la tentative. Erreur : \n" + e.Message + "\nRetentative dans 5 secondes...");
+                    System.Threading.Thread.Sleep(5000); // Sleep le Thread de 5 secondes
+
+                    try
                     {
-                        Console.WriteLine("Le serveur est en ligne.");
+                        Process();
                     }
-                    else
+
+                    catch (Exception e2)
                     {
                         Console.WriteLine("########################");
-                        Console.WriteLine("Reponse invalide.\nEnvois d'un rapport par e-mail.");
+                        Console.WriteLine("Le serveur est bel et bien hors-ligne.\nEnvois d'un rapport par e-mail.");
+                        SendMail("mateox06@hotmail.fr", e2.Message, DateTime.Now.ToString()); // Envois du rapport
                         Console.WriteLine("########################");
-                        SendMail("mateox06@hotmail.fr", "La réponse du serveur est invalide", DateTime.Now.ToString());
                     }
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine("########################");
-                    Console.WriteLine("Le serveur est hors-ligne.\nEnvois d'un rapport par e-mail.");
-                    Console.WriteLine("########################");
-                    SendMail("mateox06@hotmail.fr", e.Message, DateTime.Now.ToString());
                 }
                 finally
                 {
                     System.Threading.Thread.Sleep(time * 1000);
                 }
-                Console.WriteLine("\n");
+                Console.WriteLine("------------------------------------------------\n");
             } while (true);
         }
 
+        /// <summary>
+        /// Processus de vérification du serveur
+        /// </summary>
+        static void Process()
+        {
+            MinimalisticTelnet.TelnetConnection connection = new MinimalisticTelnet.TelnetConnection("server.osblc.fr", 10011);
+            if (connection.Read().Length > 2)
+            {
+                Console.WriteLine("Le serveur est en ligne.");
+            }
+            else
+            {
+                Console.WriteLine("########################");
+                Console.WriteLine("Reponse invalide.\nEnvois d'un rapport par e-mail.");
+                Console.WriteLine("########################");
+                SendMail("mateox06@hotmail.fr", "La réponse du serveur est invalide", DateTime.Now.ToString());
+            }
+        }
+
+        /// <summary>
+        /// Envois un mail
+        /// </summary>
+        /// <param name="destination">Adresse du destinataire</param>
+        /// <param name="error">Erreur a envoyer dans le mail</param>
+        /// <param name="date">Date de l'erreur</param>
         static void SendMail(string destination, string error, string date)
         {
-            var client = new SmtpClient("smtp.gmail.com", 587)
+            var client = new SmtpClient("smtp.gmail.com", 587) // Connexion au serveur SMTP avec l'url et le port
             {
-                Credentials = new NetworkCredential("teamspeakstats@gmail.com", "mateolepddu06"),
-                EnableSsl = true
+                Credentials = new NetworkCredential("teamspeakstats@gmail.com", "mateolepddu06"), // Entrée des données de connection
+                EnableSsl = true // Nécessaire
             };
             try
             {
+                /* Envois de l'email */
                 client.Send("teamspeakstats@gmail.com", destination, "Notification d'erreur du BOT", "Le serveur TeamSpeak semble être down d'après le BOT OSBLCDown, le " + date + ".\n" + "Mais que fait l'administration ?\n\nVoici l'erreur : \n" + error);
             }
             catch (Exception e)
             {
-                Console.WriteLine("Erreur lors de l'envoi de l'e-mail\n" + e.Message);
+                Console.WriteLine("Erreur lors de l'envoi de l'e-mail\n" + e.Message); // Erreur
             }
         }
     }
