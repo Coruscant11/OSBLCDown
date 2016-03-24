716e787d010bff4cfe606d2e11e272aa1964b20d
Coruscant11 <coruscant11@gmail.com>
2016-03-24 20:10:06 +0100

    Ajout de la gestion des erreurs avancées.

--- OSBLCDown/Program.cs
+++ OSBLCDown/Program.cs
@@ -1,22 +1,42 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Mail;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+using System.Drawing;
 
 namespace OSBLCDown
 {
     class Program
     {
+        [DllImport("user32.dll")]
+        public static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+        [DllImport("kernel32.dll")]
+        public static extern IntPtr GetConsoleWindow();
+         
+        public static int SW_HIDE = 0;
+        public static int SW_SHOW = 5; 
+
+        public static bool isShowed;
+
         static void Main(string[] args)
         {
+            Console.Title = "OSBLCDown - by Coruscant11";
+            isShowed = true;
+
+            System.Threading.Thread thread = new System.Threading.Thread(new System.Threading.ThreadStart(LoadTrayIcon));
+            thread.Start();
+
             int time = int.Parse(System.IO.File.ReadAllText("config.txt")); // Récupère l'intervalle de vérification dans le fichier config.txt
 
             Console.WriteLine("Lancement du programme avec pour paramètre " + time.ToString() + " secondes.\nAppuyez sur entrée pour valider...\n");
             Console.ReadLine();
-
+            
             do
             {
                 Console.WriteLine("------------------------------------------------");
@@ -26,20 +46,31 @@ namespace OSBLCDown
                     Process();
                 } catch (Exception e) {
 
-                    Console.WriteLine("Echec de la tentative. Erreur : \n" + e.Message + "\nRetentative dans 5 secondes...");
-                    System.Threading.Thread.Sleep(5000); // Sleep le Thread de 5 secondes
+                    int newtime = 10000;
+                    bool boucle = true;
+                    SendMail("mateox06@hotmail.fr", e.Message, DateTime.Now.ToString()); // Envois du rapport
 
-                    try
+                    while (boucle)
                     {
-                        Process();
-                    }
+                        Console.WriteLine("Echec de la tentative. Erreur : \n" + e.Message + "\nRetentative dans 10 secondes...");
+                        System.Threading.Thread.Sleep(newtime); // Sleep le Thread de 10 secondes
 
-                    catch (Exception e2)
-                    {
-                        Console.WriteLine("########################");
-                        Console.WriteLine("Le serveur est bel et bien hors-ligne.\nEnvois d'un rapport par e-mail.");
-                        SendMail("mateox06@hotmail.fr", e2.Message, DateTime.Now.ToString()); // Envois du rapport
-                        Console.WriteLine("########################");
+                        try
+                        {
+                            Process();
+                            boucle = false;
+                            Console.WriteLine("########################");
+                            Console.WriteLine("Le serveur est de nouveau en ligne. Envois d'un e-mail.");
+                            SendMailSuccess("mateox06@hotmail.fr", DateTime.Now.ToString()); // Envois du rapport
+                            Console.WriteLine("########################");
+                        }
+                        catch
+                        {
+                            Console.WriteLine("########################");
+                            Console.WriteLine("Le serveur est toujours hors-ligne.");
+                            Console.WriteLine("########################");
+                            boucle = true;
+                        }
                     }
                 }
                 finally
@@ -70,7 +101,7 @@ namespace OSBLCDown
         }
 
         /// <summary>
-        /// Envois un mail
+        /// Envois un mail d'erreur
         /// </summary>
         /// <param name="destination">Adresse du destinataire</param>
         /// <param name="error">Erreur a envoyer dans le mail</param>
@@ -85,12 +116,42 @@ namespace OSBLCDown
             try
             {
                 /* Envois de l'email */
-                client.Send("teamspeakstats@gmail.com", destination, "Notification d'erreur du BOT", "Le serveur TeamSpeak semble être down d'après le BOT OSBLCDown, le " + date + ".\n" + "Mais que fait l'administration ?\n\nVoici l'erreur : \n" + error);
+                client.Send("teamspeakstats@gmail.com", destination, "ERREUR ! - OSBLCDown pas content", "Le serveur TeamSpeak semble être down d'après le BOT OSBLCDown, le " + date + ".\n" + "Mais que fait l'administration ?\n\nVoici l'erreur : \n" + error);
             }
             catch (Exception e)
             {
                 Console.WriteLine("Erreur lors de l'envoi de l'e-mail\n" + e.Message); // Erreur
             }
+        }
+
+        /// <summary>
+        /// Envois un mail de succès
+        /// </summary>
+        /// <param name="destination">Adresse du destinataire</param>
+        /// <param name="date">Date de l'évènement</param>
+        static void SendMailSuccess(string destination, string date)
+        {
+            var client = new SmtpClient("smtp.gmail.com", 587) // Connexion au serveur SMTP avec l'url et le port
+            {
+                Credentials = new NetworkCredential("teamspeakstats@gmail.com", "mateolepddu06"), // Entrée des données de connection
+                EnableSsl = true // Nécessaire
+            };
+            try
+            {
+                /* Envois de l'email */
+                client.Send("teamspeakstats@gmail.com", destination, "C'EST BON ! - OSBLCDown content", "Le serveur TeamSpeak semble être redevenue en ligne d'après le BOT OSBLCDown, le " + date + ".\n" + "GG l'administration ?");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Erreur lors de l'envoi de l'e-mail\n" + e.Message); // Erreur
+            }
+        }
+
+        static void LoadTrayIcon()
+        {
+            Application.EnableVisualStyles();
+            new TrayIcon();
+            Application.Run();
         }
     }
 }
